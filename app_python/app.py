356c43c63718130407e8d03ac2c7998704520150
ElBatanony <ahmedelbatanony@hotmail.com>
2022-05-26 23:26:12 +0300

    Create the Moscow Time Python app

---
+++ app_python/app.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta, timezone
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def generate_moscow_time():
+    moscow_time_zone = timezone(timedelta(hours=3))
+    time = datetime.now(moscow_time_zone).strftime("%H:%M:%S")
+    return time
+
+
+@app.route("/")
+def index():
+    return f"Moscow time is { generate_moscow_time() }"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
