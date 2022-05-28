1a0fba156b218e94450e75db1dc083e40cb32ea0
NastyRu <nasty.sidenko@mail.ru>
2022-05-28 20:47:42 +0300

    Add best practices

--- app_python/main.py
+++ app_python/main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+"""Flask web application to show current Moscow time."""
 from datetime import datetime
+from flask import Flask, render_template
 import pytz
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
+    """Generate main html page."""
     cur_time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
     return render_template('time.html', moscow_time=cur_time)
 
