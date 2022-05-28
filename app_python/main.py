725cecd8ad991b8a04754f9dab30ecec7c9976f8
NastyRu <nasty.sidenko@mail.ru>
2022-05-29 00:02:48 +0300

    Add tests

--- app_python/main.py
+++ app_python/main.py
@@ -5,10 +5,16 @@ import pytz
 
 app = Flask(__name__)
 
+
+def current_moscow_time():
+    """Get current Moscow time."""
+    return datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
+
+
 @app.route('/')
 def main():
     """Generate main html page."""
-    cur_time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
+    cur_time = current_moscow_time()
     return render_template('time.html', moscow_time=cur_time)
 
 if __name__ == '__main__':
---
+++ app_python/test.py
@@ -0,0 +1,19 @@
+"""Tests for web application."""
+from datetime import datetime
+import pytest
+import pytz
+from main import app
+
+@pytest.fixture
+def client():
+    """Fixture for application."""
+    with app.test_client() as client:
+        yield client
+
+def test_time(client):
+    """Test corectness of time without seconds."""
+    resp = client.get("/")
+    time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M")
+    html_string = 'Current Moscow time (UTC+03:00): {}'.format(time)
+    assert resp.status_code == 200
+    assert html_string in str(resp.data)
