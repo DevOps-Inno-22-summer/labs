9077ab9deb4ad19fc43dcd094540c99db7467635
chernova_a <chernova_a@magnit.ru>
2022-05-30 23:38:14 +0300

    Generate secret key

---
+++ app_python/README.md
@@ -0,0 +1,5 @@
+### How to run
+
+```
+uwsgi --http :8000 --module django_project.wsgi
+```
--- app_python/django_project/django_project/settings.py
+++ app_python/django_project/django_project/settings.py
@@ -1,16 +1,12 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+
+SECRET_KEY = 'fake key'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -19,6 +15,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'django_project.urls'
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
+
+INSTALLED_APPS = [
+    'djecrety',
+]
 
 LANGUAGE_CODE = 'en-us'
 
