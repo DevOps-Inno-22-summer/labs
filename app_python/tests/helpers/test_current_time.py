9c60c772260f1710e451197570d8e2cb3ac1fe5d
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2022-06-01 14:55:11 +0300

    Update test_current_time.py

--- app_python/tests/helpers/test_current_time.py
+++ app_python/tests/helpers/test_current_time.py
@@ -5,5 +5,10 @@ from src.helpers.current_time import current_moscow_time
 def test_current_moscow_time():
     """test current moscow time"""
     offset = timezone(timedelta(hours=3))
-    assert current_moscow_time() == datetime.now(offset)
+    gotten_response = str(current_moscow_time()).split(" ")
+    expected_response = str(datetime.now(offset)).split(" ")
+    assert gotten_response[0] == expected_response[0]
+    gotten_response = gotten_response[1].split(".")
+    expected_response = expected_response[1].split(".")
+    assert gotten_response[0] == expected_response[0]
     
