7b64674259ec4c13e3ccbb56d681741e6077265c
AlxGration <sashamiko@bk.ru>
2022-06-02 12:14:28 +0300

    satisfy code to linter

--- app_python/moscow_time.py
+++ app_python/moscow_time.py
@@ -12,6 +12,7 @@ def moscow():
     localDatetime = datetime.now().astimezone(pytz.timezone(tzone))
     return localDatetime.strftime("%Y-%m-%d %H:%M:%S")
 
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
--- app_python/moscow_time_test.py
+++ app_python/moscow_time_test.py
@@ -3,12 +3,13 @@ from datetime import datetime
 import pytz
 from moscow_time import moscow
 
+
 class TestTime(unittest.TestCase):
+
 
     def test_moscow(self):
         tzone = 'Europe/Moscow'
         localDatetime = datetime.now().astimezone(pytz.timezone(tzone))
         expected = localDatetime.strftime("%Y-%m-%d %H:%M:%S")
-        
         actual = moscow()
         self.assertEqual(expected, actual, 'Failed on moscow_test')
