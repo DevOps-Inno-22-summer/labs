e4eb70a19c9e694c7ae0252a783fd9d0455eee5d
ElBatanony <ahmedelbatanony@hotmail.com>
2022-06-06 06:59:56 +0300

    Add unit tests to Python project
    Includes tests for time generation, HTTP response, and time formatting
    
    Tests are ran via pytest

--- app_python/app_test.py
+++ app_python/app_test.py
@@ -1,17 +1,19 @@
+from datetime import datetime
+
 import app
 
 
-def test_time_generation():
-    time = app.generate_moscow_time()
-    assert ":" in time
+class TestApp:
 
+    def test_time_generation(self):
+        time = app.generate_moscow_time()
+        assert ":" in time
 
-def test_http_response():
-    response = app.index()
-    assert "Moscow time is" in response
+    def test_http_response(self):
+        response = app.index()
+        assert "Moscow time is" in response
 
-
-if __name__ == "__main__":
-    test_time_generation()
-    test_http_response()
-    print("Tests passed")
+    def test_time_formatting(self):
+        assert app.format_time(datetime(2022, 6, 6, 23, 55, 3)) == "23:55:03"
+        assert app.format_time(datetime(2022, 6, 6, 1, 1, 0)) == "01:01:00"
+        assert app.format_time(datetime(2022, 6, 6, 13, 0, 22)) == "13:00:22"
