a032ebddb983d62b5cdfde0b80c1288d90b6110d
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2022-06-07 00:45:19 +0300

    Add protection rules

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -14,3 +14,22 @@ resource "github_branch_default" "default"{
   branch     = "master"
 }
 
+#Protect the master branch of the labs repository. Only allow a specific user to merge to the branch.
+resource "github_branch_protection_v3" "labs" {
+  repository     = github_repository.labs.name
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["ci/travis"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews = true
+    dismissal_users       = ["raghadsalameh1"]
+    dismissal_teams       = []
+  }
+}
+
+
