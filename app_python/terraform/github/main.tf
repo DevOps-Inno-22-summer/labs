de64bfc3398751e5485cfbcf2b13d813f3bb2ce5
ElBatanony <ahmedelbatanony@hotmail.com>
2022-06-07 06:38:10 +0300

    Add GitHub Terraform config file

---
+++ app_python/terraform/github/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.0"
+    }
+  }
+}
+
+variable "github_token" {
+  type        = string
+  description = "GitHub Access Token"
+}
+
+provider "github" {
+  token = var.github_token
+  owner = "elbatanony-devops"
+}
+
+resource "github_repository" "devops-terraform" {
+  name        = "devops-terraform"
+  description = "Solutions to DevOps labs"
+  visibility  = "public"
+}
+
+resource "github_branch_default" "default" {
+  repository = github_repository.devops-terraform.name
+  branch     = "main"
+}
+
+resource "github_branch_protection_v3" "branch_protection" {
+  repository = github_repository.devops-terraform.name
+  branch     = "main"
+
+  restrictions {
+    users = ["ElBatanony"]
+  }
+}
+
+resource "github_membership" "membership_for_myself" {
+  username = "ElBatanony"
+  role     = "admin"
+}
+
+resource "github_membership" "membership_for_insaf" {
+  username = "safinsaf"
+  role     = "admin"
+}
+
+resource "github_team" "admins_team" {
+  name    = "admins-team"
+  privacy = "closed"
+}
+
+resource "github_team" "members_team" {
+  name    = "members-team"
+  privacy = "closed"
+}
+
+resource "github_team_members" "admins_team_members" {
+  team_id = github_team.admins_team.id
+
+  members {
+    username = "ElBatanony"
+    role     = "maintainer"
+  }
+
+  members {
+    username = "safinsaf"
+    role     = "maintainer"
+  }
+}
