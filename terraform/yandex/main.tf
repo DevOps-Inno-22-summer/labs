c5c03f0c2afbf0512cd6c8777613c9944a1acb4e
Ramin <raminefendi@gmail.com>
2022-06-12 16:02:42 +0300

    Terraform for yandex added

---
+++ terraform/yandex/main.tf
@@ -0,0 +1,98 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = var.y_token
+  cloud_id  = var.y_cloud_id
+  folder_id = var.y_folder_id
+  zone      = "ru-central1-a"
+}
+
+# provider "yandex" {
+#   token     = "AQAAAAA3oFrcAATuwUYZDtskgEVyt9PE2rYHfpo"
+#   cloud_id  = "b1gnqr5ta6mn3ugrbvk3"
+#   folder_id = "b1gfhsslotg20fdvni58"
+#   zone      = "ru-central1-a"
+# }
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8dqiigupbmq398h0va"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8dqiigupbmq398h0va"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
---
+++ terraform/yandex/variables.tf
@@ -0,0 +1,17 @@
+variable "y_token" {
+  description = "OAuth token"
+  type        = string
+  sensitive   = true
+}
+
+variable "y_cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "y_folder_id" {
+  description = "Yandex Folder ID"
+  type        = string
+  sensitive   = true
+}
