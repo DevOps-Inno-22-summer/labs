e2613962e0c0ad43bb36bc40a23bd664f0855bcf
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2022-06-14 01:43:44 +0300

    Add security rule to terraform for allowing http

--- terraform/dev/app_python/azure.tf
+++ terraform/dev/app_python/azure.tf
@@ -45,12 +45,24 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "SSH"
-    priority                   = 1001
+    priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8000"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
