76045b9d6b332c681b08b1f8a5cb04708d7c89b1
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2022-06-28 01:07:17 +0300

    Add visits endpoint

--- app_python/src/helpers/current_time.py
+++ app_python/src/helpers/current_time.py
@@ -5,3 +5,14 @@ def current_moscow_time()-> datetime:
     """Current Moscow time"""
     offset = timezone(timedelta(hours=3))
     return datetime.now(offset)
+
+def record_access():
+    """ record access to root pass by a client"""
+    with open('visits.txt', 'a',encoding='UTF-8') as file:
+        file.write(f'The root pass is accessed by a client at: {datetime.now()}')
+
+def get_visits_content():
+    """ Get content of visits file """
+    with open('visits.txt', encoding='UTF-8') as file:
+        contents = file.read()
+    return contents
