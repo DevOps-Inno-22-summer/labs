bbf65b5f6eb9e862ad11a0f04f367057fa2be966
Vitalii Smolev <smolev.vitaliy@gmail.com>
2022-06-27 23:17:59 +0300

    added terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,25 @@
 terraform {
   required_providers {
-    vagrant = {
-      source  = "bmatcuk/vagrant"
-      version = "~> 4.0.0"
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
     }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  token = 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
+}
+
+resource "github_repository" "mainrepo" {
+  name        = "waitme"
+  description = "My awesome time app"
+
+  visibility = "public"
+
+  template {
+    owner      = "github"
+    repository = "terraform-module-template"
   }
 }
