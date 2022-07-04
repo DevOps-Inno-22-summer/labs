37068d1c93454c5506c8185b63be197381a97add
Willsem <happymainer@gmail.com>
2022-07-04 22:26:38 +0300

    lab12

--- app_python/app.py
+++ app_python/app.py
@@ -1,7 +1,9 @@
 '''Run web application'''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from local_time import get_current_time
+
+LOGS_FILENAME = "logs/visits.log"
 
 app = Flask(__name__)
 
@@ -9,7 +11,20 @@ app = Flask(__name__)
 def index():
     '''Main page of web application'''
     time = get_current_time(3)
+    with open(LOGS_FILENAME, "a", encoding="UTF-8") as file:
+        addr = str(request.remote_addr)
+        file.write(addr + " : " + time + "\n")
     return render_template("index.html", result=time)
+
+
+@app.route("/visits", methods=["GET"])
+def visits():
+    '''Page which show the content of file with visited users'''
+    result = ""
+    with open(LOGS_FILENAME, "r", encoding="UTF-8") as file:
+        for line in file:
+            result += line + "<br />"
+    return result
 
 
 if __name__ == "__main__":
