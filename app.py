ef636b5dc8fd72632f8138655b21c1cbd057c70e
Helen Iudina <Iydina13@mail.ru>
2022-07-05 01:09:52 +0300

    Lab 12

--- app.py
+++ app.py
@@ -11,7 +11,21 @@ def home():
     time_zone = timezone(timedelta(hours=3))
     date = datetime.now(time_zone)
 
+    with open("data/logs.txt", "a") as file:
+        print(date, file=file)
+
     return render_template("home.html", result=date)
+
+
+@app.route("/visits", methods=["GET"])
+def visits():
+    '''Function 2'''
+    page = ""
+    with open("data/logs.txt", "r", encoding="UTF-8") as file:
+        for line in file:
+            page += line + "<br>"
+    return page
+
 
 if __name__ == "__main__":
     app.run(debug=True)
