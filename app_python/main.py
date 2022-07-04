e77ed2829c4fd1b538043f9c6faa5e48762527e8
Ilya Nokhrin <73830219+MrTuxedoCrab@users.noreply.github.com>
2022-07-04 19:27:25 +0500

    Lab 12 done

--- app_python/main.py
+++ app_python/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from get_time import GetTime
+from logger import Logger
 
 app = Flask(__name__)
 
@@ -7,7 +8,13 @@ app = Flask(__name__)
 def index():
     msk_time = GetTime.get_moscow_time()
     msk_time_api = GetTime.get_moscow_time_api()
+    Logger.log_visit()
     return render_template('index.html', msk_time = msk_time, msk_time_api = msk_time_api)
+
+@app.route("/visits")
+def visits():
+    Logger.log_visit('/visits')
+    return render_template('visits.html', visits = Logger.get_visits())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
