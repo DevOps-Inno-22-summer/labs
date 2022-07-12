5e665eaca45bf58c3ef441716f69a5c730b1253c
yaroslav_yudi <yaskih@mail.ru>
2022-07-12 06:43:10 +0300

    fix python app

--- app_python/app/services/visits.py
+++ app_python/app/services/visits.py
@@ -1,21 +1,24 @@
 from app.services.moscow_time import MoscowTime
+import os
 
 
 class Visits:
 
     def __init__(self):
+        fileDir = os.path.dirname(os.path.realpath('__file__'))
+        self.visitsFileName = os.path.join(fileDir, 'logs/visits.log')
         self.moscow_time = MoscowTime()
 
     def add_visit(self):
         time = self.moscow_time.now()
-        with open('logs/visits.log', 'a') as fp:
+        with open(self.visitsFileName, 'a') as fp:
             fp.write(time)
             fp.write('\n')
             return time
 
     def get_visits(self):
-        with open('logs/visits.log', 'r') as fp:
-            return fp.read()
+        with open(self.visitsFileName, 'r') as fp:
+            return fp.read().split('\n')
         
 
 
