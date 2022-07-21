fc7570b06ed5ead5a765cb1cb918a0202a6400be
yaroslav_yudi <yaskih@mail.ru>
2022-07-21 18:36:20 +0400

    add /metrics to python app

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -4,12 +4,16 @@ from app.api.api_v1.api import api_router
 from app.core.config import settings
 from app.api.api import router as root_router
 
+from starlette_prometheus import metrics, PrometheusMiddleware
+
 # root_router = APIRouter()
 app = FastAPI(title="API")
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(root_router)
 
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
