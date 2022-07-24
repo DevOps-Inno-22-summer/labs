8d63e7cb145f0a2ba85eb22da38837ca59a5ca44
Abdurasul Rakhimov <rakhimovabdurasul@gmail.com>
2022-07-24 14:22:28 +0300

    lab12 full

--- app_python/app.py
+++ app_python/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from datetime import datetime
 import pytz
+import logging
+
+
+logging.basicConfig(filename='logs.txt',
+                    encoding='utf-8', level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -9,7 +14,16 @@ app = Flask(__name__)
 def current_time():
     moscow_timezone = pytz.timezone('Europe/Moscow')
     datetime_moscow = datetime.now(moscow_timezone)
+    logging.debug('main function called :' + datetime_moscow)
     return f"Moscow: {datetime_moscow.strftime('%Y:%m:%d %H:%M:%S %Z %z')}"
+
+
+@app.route('/visits')
+def current_time():
+    logs = ''
+    with open('logs.txt', 'r') as f:
+        logs = f.read()
+    return logs
 
 
 if __name__ == '__main__':
